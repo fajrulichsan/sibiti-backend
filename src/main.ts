febdaf79d0bbec3b4f0862e7117e8a58a9657e59
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-04-18 00:27:55 +0700

    edit cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,6 @@ import * as cors from 'cors';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule , {cors : true});
-  app.use(cors({
-    origin: 'https://sibiti-frontend.netlify.app', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
   await app.listen(3000);
 }
 bootstrap();
