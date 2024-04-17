f125c9921cbb92541347b86d5041c5d3ee2f14f0
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-04-18 00:38:34 +0700

    fix cors v2

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,8 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors : true});
 
-  app.use(cors({
-    origin: 'https://sibiti-frontend.netlify.app', // Ganti dengan URL Frontend Anda
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Izinkan penggunaan kredensial (misalnya, cookie)
-  }));
   await app.listen(3000);
 }
 bootstrap();
