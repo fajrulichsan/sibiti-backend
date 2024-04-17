c6aacee9ee41c8a6cc650eadacd61efc8b7b6a54
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-04-18 00:42:56 +0700

    cors false

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule, {cors : true});
+  const app = await NestFactory.create(AppModule, {cors : false});
 
   await app.listen(3000);
 }
