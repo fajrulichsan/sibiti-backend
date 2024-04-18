f3113a99295a3f8ab097fbced21f66b2163bdb0f
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-04-18 16:39:22 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-
-  // CORS middleware
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200); // Respond to OPTIONS requests with 200 OK
-    } else {
-      next(); // Continue to the next middleware
-    }
+  app.use(function (request: Request, response: Response, next: NextFunction) {
+    response.setHeader('Access-Control-Allow-Origin', 'https://sibiti-frontend.vercel.app');
+    next();
   });
-
   await app.listen(3000);
 
   
