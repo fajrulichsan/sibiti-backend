da1ccde4aa42d9492c39eeb83591e53bd346e724
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-04-18 14:39:29 +0700

    fix cros

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors : true});
   app.enableCors({
     origin: [
       'https://sibiti-frontend.netlify.app/'
