78029cbae30d8b8e7f802924e8b1541072a66653
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-04-18 11:57:02 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule, {cors : false});
-
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
