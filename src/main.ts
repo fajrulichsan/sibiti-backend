fc62dd44f1a55b45fa19d9329a505e8fc580d3d7
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-04-18 16:09:51 +0700

    fix cors v.11

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,18 @@ import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: '*', // Replace with allowed origin(s)
-      allowedHeaders: ['Content-Type', 'Authorization'], // Allowed request headers
-      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-      credentials : true
-    },
+  const app = await NestFactory.create(AppModule);
+
+  // CORS middleware
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200); // Respond to OPTIONS requests with 200 OK
+    } else {
+      next(); // Continue to the next middleware
+    }
   });
 
   await app.listen(3000);
