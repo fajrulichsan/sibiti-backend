acfed99a859e3db00c9c9aa4c78ba48ab1bb4e86
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-04-18 19:44:32 +0700

    fix cores v.12

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
-  app.use(function (request: Request, response: Response, next: NextFunction) {
-    response.setHeader('Access-Control-Allow-Origin', 'https://sibiti-frontend.vercel.app');
-    next();
-  });
+  const app = await NestFactory.create(AppModule, {cors: true});
   await app.listen(3000);
 
   
